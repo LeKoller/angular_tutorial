ac3043ae0304773048b4ec421f9f32478d5bc83e
Leandro Koller <lekoller@yahoo.com>
2021-05-15 20:51:32 -0300

    feat: Add task reader of json server.

---
+++ src/app/components/task-item/task-item.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Task } from '../../mock.tasks';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-task-item',
+  templateUrl: './task-item.component.html',
+  styleUrls: ['./task-item.component.scss'],
+})
+export class TaskItemComponent implements OnInit {
+  @Input() task: Task;
+  faTimes = faTimes;
+
+  constructor() {
+    this.task = {
+      text: '',
+      day: '',
+      reminder: false,
+    };
+  }
+
+  ngOnInit(): void {}
+
+  onDelete(task: Task): void {
+    console.log(task);
+  }
+}
